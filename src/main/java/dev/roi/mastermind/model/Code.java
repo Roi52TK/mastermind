d8653a72a1f943165166069bcd8e7891c88e7cab
Roi52TK <roitheknight52@gmail.com>
2026-09-10 14:32:15 +0300

    Implemented Code data class, and random code generator.

--- src/main/java/dev/roi/mastermind/model/Code.java
+++ src/main/java/dev/roi/mastermind/model/Code.java
@@ -1,4 +1,30 @@
 package dev.roi.mastermind.model;
 
 public class Code {
+
+    private final int[] code;
+    private final int length;
+    private final int optionsCount;
+
+    public Code(int length, int optionsCount) {
+        this.code = new int[length];
+        this.length = length;
+        this.optionsCount = optionsCount;
+    }
+
+    public int get(int index) {
+        return code[index];
+    }
+
+    public void set(int index, int value) {
+        code[index] = value;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getOptionsCount() {
+        return optionsCount;
+    }
 }
---
+++ src/main/java/dev/roi/mastermind/model/CodeGenerator.java
@@ -0,0 +1,21 @@
+package dev.roi.mastermind.model;
+
+import java.util.Random;
+
+public final class CodeGenerator {
+    private static final Random rand = new Random();
+
+    private CodeGenerator() {}
+
+    public static Code randomCode(int length, int optionsCount) {
+        Code code = new Code(length, optionsCount);
+        int value;
+
+        for(int i = 0; i < length; i++) {
+            value = rand.nextInt(optionsCount);
+            code.set(i, value);
+        }
+
+        return code;
+    }
+}
